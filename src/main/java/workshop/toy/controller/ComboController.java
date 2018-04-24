ea4b3b7517d30cbf88c6e75de5b9f9bca0c0bd0f
Nathas Yingsukamol <new08242@gmail.com>
2018-04-24 16:17:06 +0700

    add gender combo

--- src/main/java/workshop/toy/controller/ComboController.java
+++ src/main/java/workshop/toy/controller/ComboController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import workshop.toy.model.Combo;
 import workshop.toy.repo.AgeRepo;
+import workshop.toy.repo.GenderRepo;
 
 import java.util.List;
 
@@ -12,9 +13,16 @@ import java.util.List;
 public class ComboController {
     @Autowired
     private AgeRepo ageRepo;
+    @Autowired
+    private GenderRepo genderRepo;
 
     @GetMapping("/rest/getAgeCombo")
     public List<Combo> getAgeCombo(){
         return ageRepo.searchAgeCombo();
+    }
+
+    @GetMapping("/rest/getGenderCombo")
+    public List<Combo> getGenderCombo(){
+        return genderRepo.searchGenderCombo();
     }
 }
---
+++ src/main/java/workshop/toy/repo/GenderRepo.java
@@ -0,0 +1,12 @@
+package workshop.toy.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import workshop.toy.model.Combo;
+import org.springframework.data.jdbc.repository.query.Query;
+
+import java.util.List;
+
+public interface GenderRepo extends CrudRepository<Combo,Integer> {
+    @Query("select gender_id as value , description as text from gender")
+    List<Combo> searchGenderCombo();
+}
