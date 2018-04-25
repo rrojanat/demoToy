413427951509488acf79a337f27e303879f68e36
tanat <tanatloke@gmail.com>
2018-04-25 11:32:07 +0700

    Add Model Criteria and New Search Repo

--- src/main/java/workshop/toy/controller/ToyController.java
+++ src/main/java/workshop/toy/controller/ToyController.java
@@ -1,14 +1,13 @@
 package workshop.toy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import workshop.toy.model.Toy;
+import workshop.toy.model.ToyCriteria;
 import workshop.toy.repo.ToyRepo;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 public class ToyController {
@@ -21,4 +20,26 @@ public class ToyController {
         return toyRepo.getToyById(new BigDecimal(toyId));
     }
 
+    @PostMapping("/rest/toy")
+    @ResponseBody
+    public List<Toy> searchToy(@RequestBody ToyCriteria toyCriteria) {
+        String ageId = toyCriteria.getSearchAge();
+        String genderId = toyCriteria.getSearchGender();
+        if (isEmptyString(ageId) && isEmptyString(genderId)) {
+            return toyRepo.searchToyByAgeAndGender(ageId, genderId);
+        } else if (isEmptyString(ageId) && !isEmptyString(genderId)) {
+            return toyRepo.searchToyByGender(genderId);
+        } else if (!isEmptyString(ageId) && isEmptyString(genderId)) {
+            return toyRepo.searchToyByAge(ageId);
+        }
+
+        return toyRepo.searchToy();
+    }
+
+    private boolean isEmptyString(String input) {
+        if (input == null || input.equalsIgnoreCase("")) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/workshop/toy/repo/ToyRepo.java
+++ src/main/java/workshop/toy/repo/ToyRepo.java
@@ -6,9 +6,22 @@ import org.springframework.data.repository.query.Param;
 import workshop.toy.model.Toy;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface ToyRepo extends CrudRepository<Toy,Integer> {
 
     @Query("select a.toy_id, a.name, b.description as gender, c.description as age, a.price, a.brand, a.qty, case when a.qty = 0 then 'Out of Stock' else 'In Stock' end stockStatus, 'Cash on Delivery' as shippingMethod, a.toy_img from toy a, gender b, age c where a.gender_id = b.gender_id and a.age_id = c.age_id and a.toy_id = :toyId")
     Toy getToyById(@Param("toyId") BigDecimal toyId);
+
+    @Query("select a.toy_id, a.name, b.description as gender, c.description as age, a.price, a.brand, a.qty, case when a.qty = 0 then 'Out of Stock' else 'In Stock' end stockStatus, 'Cash on Delivery' as shippingMethod, a.toy_img from toy a, gender b, age c where a.gender_id = b.gender_id and a.age_id = c.age_id")
+    List<Toy> searchToy();
+
+    @Query("select a.toy_id, a.name, b.description as gender, c.description as age, a.price, a.brand, a.qty, case when a.qty = 0 then 'Out of Stock' else 'In Stock' end stockStatus, 'Cash on Delivery' as shippingMethod, a.toy_img from toy a, gender b, age c where a.gender_id = b.gender_id and a.age_id = c.age_id and a.age_id = :ageId and a.gender_id = :genderId")
+    List<Toy> searchToyByAgeAndGender(@Param("ageId") String ageId, @Param("genderId") String genderId);
+
+    @Query("select a.toy_id, a.name, b.description as gender, c.description as age, a.price, a.brand, a.qty, case when a.qty = 0 then 'Out of Stock' else 'In Stock' end stockStatus, 'Cash on Delivery' as shippingMethod, a.toy_img from toy a, gender b, age c where a.gender_id = b.gender_id and a.age_id = c.age_id and a.gender_id = :genderId")
+    List<Toy> searchToyByGender(@Param("genderId") String genderId);
+
+    @Query("select a.toy_id, a.name, b.description as gender, c.description as age, a.price, a.brand, a.qty, case when a.qty = 0 then 'Out of Stock' else 'In Stock' end stockStatus, 'Cash on Delivery' as shippingMethod, a.toy_img from toy a, gender b, age c where a.gender_id = b.gender_id and a.age_id = c.age_id and a.age_id = :ageId")
+    List<Toy> searchToyByAge(@Param("ageId") String ageId);
 }
